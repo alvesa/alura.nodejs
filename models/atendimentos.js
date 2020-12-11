1ad31ac815af28eedd1b59efad5c7a9b7589177c
Andre Euclides Alves <andreeuclidesalves@MacBook-Pro-de-Andre.local>
2020-12-11 11:31:00 -0300

    validando se nas infos enviadas há erros

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -6,15 +6,42 @@ class Atendimento {
     {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
-        const atendimentoDatado = {... atendimento, dataCriacao, data};
-        const sql = 'INSERT INTO Atendimentos SET ?';
 
-        conexao.query(sql, atendimentoDatado, (err, resultado) => {
-            if(err)
-                res.status(400).json(err);
-            else
-                res.status(201).json(resultado);
-        });
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+        const clienteEhValido = atendimento.cliente.length >= 5;
+
+        const atendimentoDatado = {... atendimento, dataCriacao, data};
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
+            }
+        ];
+
+        const erros = validacoes.filter(campo => !campo.valido);
+        const existemErros = erros.length;
+
+        if(existemErros)
+            res.status(400).json(erros);
+        else
+        {
+            const sql = 'INSERT INTO Atendimentos SET ?';
+    
+            conexao.query(sql, atendimentoDatado, (err, resultado) => {
+                if(err)
+                    res.status(400).json(err);
+                else
+                    res.status(201).json(resultado);
+            });
+        }
+
     }
 }
 
