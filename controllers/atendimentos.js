8af4c80f54cd4e63ec60652621a6bb36ffee67e8
Andre Euclides Alves <andreeuclidesalves@MacBook-Pro-de-Andre.local>
2020-12-11 10:29:38 -0300

    adicionando body-parser, para aceitar urlencoded e json

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,4 +2,9 @@ module.exports = app => {
     app.get('/atendimentos', (req, res) => {
         res.send('voce esta na rota de atendimentos');
     });
+
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body);
+        res.send('Voce está na rota de atendimentos e está realizando um POST');
+    });
 };
