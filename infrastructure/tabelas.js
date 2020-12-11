c6ca2ae240314bd50ae46fab3e0c4addecfa8b89
Andre Euclides Alves <andreeuclidesalves@MacBook-Pro-de-Andre.local>
2020-12-11 11:11:18 -0300

    adicionando os campos de data e formatando os mesmos usando moment

--- infrastructure/tabelas.js
+++ infrastructure/tabelas.js
@@ -8,7 +8,7 @@ class Tabelas {
 
     criarAtendimentos()
     {
-        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente VARCHAR(50) NOT NULL, pet VARCHAR(20), servico VARCHAR(20) NOT NULL, status VARCHAR(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
+        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente VARCHAR(50) NOT NULL, pet VARCHAR(20), servico VARCHAR(20) NOT NULL, data DATETIME NOT NULL, dataCriacao DATETIME NOT NULL, status VARCHAR(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
         this.conexao.query(sql, (err, ) =>{
             if(err)
                 console.log(err);
